ee8bf26f0baa93fd255bd1d5d235db4e69e9f7ec
ColtenBegle <46980524+ColtenBegle@users.noreply.github.com>
2020-08-30 15:59:00 -0500

    Importing project

---
+++ Quicksort Algorigthm/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Quicksort_Algorigthm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            char[] responses = new char[] { 'Y', 'y', 'N', 'n' };
+            char cont = responses[0];
+            while (cont == responses[0] || cont == responses[1])
+            {
+                Console.Write("How many numbers do you want to sort: ");
+                string ans = Console.ReadLine();
+                bool ansCheck = int.TryParse(ans, out int value);
+                if (value == 0 || ansCheck == false)
+                {
+                    Console.WriteLine("Invalid amount.");
+                }
+                else
+                {
+                    Queue<int> quickQueue = new Queue<int>();
+                    QuickSort oldSort = new QuickSort();
+                    for (int i = 0; i < value; i++)
+                    {
+                        quickQueue.Enqueue(rand.Next(0, 1000));
+                    }
+                    Console.Write("Unsorted:");
+                    printQueueu(quickQueue);
+
+                    Console.WriteLine("\n");
+
+                    QuickSort newSort = new QuickSort();
+                    newSort.QuickSortAlgo(ref quickQueue, quickQueue.Last());
+                    Console.Write("Sorted: ");
+                    printQueueu(quickQueue);
+                    Console.WriteLine();
+                }
+                Console.Write("Would you like to sort again (Y/N): ");
+                string returned = Console.ReadLine();
+                while (Char.TryParse(returned, out char result) == false || inResponses(responses, result) == false)
+                {
+                    Console.Write("Incorrect input! Enter Y/N: ");
+                    returned = Console.ReadLine();
+                }
+                Char.TryParse(returned, out char newAns);
+                cont = newAns;
+                Console.WriteLine();
+            }
+        }
+
+        static bool inResponses(char[] responses, char input)
+        {
+            bool test = responses.Contains(input);
+            return test;
+        }
+
+        static void printQueueu(Queue<int> S)
+        {
+            foreach (Object obj in S)
+            {
+                Console.Write("|{0}|", obj);
+            }
+        }
+    }
+}
---
+++ Quicksort Algorigthm/QuickSort.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quicksort_Algorigthm
+{
+    class QuickSort
+    {
+        public void QuickSortAlgo(ref Queue<int> S, int p)
+        {
+            if (S.Count > 1)
+            {
+                Queue<int> L = new Queue<int>();
+                Queue<int> E = new Queue<int>();
+                Queue<int> G = new Queue<int>();
+
+                partition(S, p, ref L, ref E, ref G);
+                QuickSortAlgo(ref L, findP(L));
+                QuickSortAlgo(ref G, findP(G));
+
+                S = mergeQueues(ref L, ref E, ref G);
+            }
+        }
+        static void partition(Queue<int> S, int p, ref Queue<int> L, ref Queue<int> E, ref Queue<int> G)
+        {
+            while (S.Count != 0)
+            {
+                int y = S.Dequeue();
+                if (y < p)
+                {
+                    L.Enqueue(y);
+                }
+                else if (y == p)
+                {
+                    E.Enqueue(y);
+                }
+                else
+                    G.Enqueue(y);
+            }
+        }
+
+        static Queue<int> mergeQueues(ref Queue<int> L, ref Queue<int> E, ref Queue<int> G)
+        {
+            Queue<int> newQ = new Queue<int>();
+            while (L.Count > 0)
+            {
+                newQ.Enqueue(L.Dequeue());
+            }
+            while (E.Count > 0)
+            {
+                newQ.Enqueue(E.Dequeue());
+            }
+            while (G.Count > 0)
+            {
+                newQ.Enqueue(G.Dequeue());
+            }
+            return newQ;
+        }
+
+        static int findP(Queue<int> S)
+        {
+            if (S.Count > 1)
+            {
+                int p = S.Last();
+                return p;
+            }
+            else
+                return 0;
+        }
+    }
+}
